900c56756d1b8e11f114f9bb11b90267259d5d00
OceanPig <wweiooogo@gmail.com>
2026-08-13 23:48:36 +0800

    Site updated: 2026-08-13 23:48:35

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,9 @@ function toggleMenu() {
     var menuButton = document.getElementById("menu-btn");  
     if(menuList.classList.contains("active")){
       menuList.classList.remove("active");
-      menuButton.innerHTML = "Menu";
+      menuButton.innerHTML = '<span class="menu-toggle-symbol" aria-hidden="true"></span><span>菜单</span>';
     }else{
       menuList.classList.add("active");
-      menuButton.innerHTML = "<div class=\"icon arrow-up\"> </div>";
+      menuButton.innerHTML = '<span class="menu-toggle-symbol menu-toggle-symbol-open" aria-hidden="true"></span><span>收起</span>';
     }
 }
